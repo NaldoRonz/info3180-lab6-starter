b6cee95478b9ec0de1dc96fe28e96e3f77f601d4
NaldoRonz <ronaldomreid19@gmail.com>
2020-04-17 22:53:58 -0500

    Lab 6

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -41,11 +41,71 @@ Vue.component('app-footer', {
     }
 })
 
+Vue.component('news-list', {
+    template: `
+        <div class="news">
+             <h2>News</h2>
+                <div class="form-inline d-flex justify-content-center">
+                  <div class="form-group mx-sm-3 mb-2">
+                           <label class="sr-only" for="search">Search</label>
+                                    <input type="search" name="search" v-model="searchTerm"id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+                                            <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+                  </div>
+                </div> 
+             <ul class="news__list">
+                <div class ="grid-container">
+                  <li v-for="article in articles" class="news__item"> 
+                    <div class ="grid-item">
+                       {{ article.title }}</br>
+                      <img v-bind:src="article.urlToImage" class = "article_image"/></br>
+                      {{article.description}}
+                    </div>
+                   </li>
+                </div>
+             </ul>
+        </div>
+      `,  
+
+    created: function() {
+      let self = this;
+      fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=b8e9e495663840069e9d5320a8999ae5')
+      .then(function(response) {
+            return response.json();
+             }) 
+      .then(function(data) { 
+            console.log(data);
+            self.articles = data.articles;
+             });
+      },
+
+    data: function() {
+        return { articles: [], searchTerm: ''}
+      },
+        methods: { 
+          searchNews: function() {
+           let self = this;
+             fetch('https://newsapi.org/v2/everything?q='+ self.searchTerm + '&language=en&apiKey=b8e9e495663840069e9d5320a8999ae5')
+               .then(function(response) {
+                 return response.json();
+                 })
+              .then(function(data) {
+              console.log(data);
+             self.articles = data.articles;
+                });
+         }
+       }
+
+ });
+
 
 let app = new Vue({
     el: '#app',
     data: {
         welcome: 'Hello World! Welcome to VueJS'
     }
+
 });
+
+
+
 
